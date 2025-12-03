6d2e0286dcc6e2b462e2de5f7294bfbb63838331
Del <6117352+sycomix@users.noreply.github.com>
2025-12-02 19:18:43 -0600

    Move assembly metadata from AssemblyInfo.cs to csproj
    Assembly metadata attributes such as title, product, copyright, and version information were removed from AssemblyInfo.cs and added to the AC.BLL.Models.csproj file. This centralizes assembly information in the project file, aligning with modern .NET practices.

--- AC.BLL.Models/Properties/AssemblyInfo.cs
+++ AC.BLL.Models/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("AC.BLL.Models")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AC.BLL.Models")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("db33a1ac-d347-4ae7-b7a3-6bdad84bc62b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

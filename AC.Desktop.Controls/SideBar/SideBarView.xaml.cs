7708b217aa09654091811ff8b2bee12964343259
Del <6117352+sycomix@users.noreply.github.com>
2025-12-02 19:24:47 -0600

    * 'Line 28: Removed 'this.' qualifier from 'InitializeComponent()' to match the generated partial method signature for WPF user controls, which is typically available as a parameterless method in the partial class.' in file 'AC.Desktop.Controls\SideBar\SideBarView.xaml.cs'

--- AC.Desktop.Controls/SideBar/SideBarView.xaml.cs
+++ AC.Desktop.Controls/SideBar/SideBarView.xaml.cs
@@ -25,7 +25,7 @@ namespace AC.Desktop.Controls.SideBar
     {
         public SideBarView()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
     }
 }
